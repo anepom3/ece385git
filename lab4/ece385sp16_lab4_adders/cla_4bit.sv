5a30ff92d50d3a6cf9c5ff854cd513d9eafd34f0
tpm2 <tpm2@illinois.edu>
2020-02-10 01:49:43 -0600

    Beginnings of workings on CLA

---
+++ lab4/ece385sp16_lab4_adders/cla_4bit.sv
@@ -0,0 +1,27 @@
+module cla_4bit
+(
+    input   logic[3:0]     A,
+    input   logic[3:0]     B,
+    output  logic[3:0]     Sum,
+    output  logic           CO,
+	 output  logic          Pg,
+	 output  logic          Gg
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+endmodule
