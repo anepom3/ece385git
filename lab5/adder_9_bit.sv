999690abbdec3026c49f2ce7724c05334922affb
anepom3 <anepom3@illinois.edu>
2020-02-15 22:00:59 -0600

    Worked on adder_9_bit.sv and control.sv

--- lab5/adder_9_bit.sv
+++ lab5/adder_9_bit.sv
@@ -1,59 +1,68 @@
 module adder_9_bit
 (
-    input   logic[7:0]     A,
-    input   logic[7:0]     B,
-	 input	logic				A_9th
-	 input	logic				B_9th
-	 input	logic				select_op
-    output  logic[7:0]     Sum,
-	 output	logic				Sum_9th
-	 output	logic				B_Sum
-    output  logic          cout
+  input   logic[7:0]     S,
+  input   logic[7:0]     A,
+  input	  logic				   S_9th,
+  input	  logic				   A_9th,
+  input	  logic				   select_op,
+  input   logic          M,
+  output  logic[7:0]     Final_Sum,
+  output	logic				   Final_Sum_9th,
+  output  logic          COUT
 );
 
-    /* 
+    /*
      * This implements the 9-bit adder using a two, 4-bit Carry Ripple Adders, and one
      * Full Adder for the 9th bit.
      * This implementation is completly combinational (it doesn't use always_ff or always_latch).
      */
-		logic [7:0] A';
-		logic [7:0] B';
-		logic A_9th';
-		logic B_9th';
+		logic [7:0] S';
+		logic S_9th';
 		logic cin;
-		
-		logic C0, C1;
-		
-		always_comb begin
-			A'[0] = A[0] XOR select_op;
-			A'[1] = A[1] XOR select_op;
-			A'[2] = A[2] XOR select_op;
-			A'[3] = A[3] XOR select_op;
-			A'[4] = A[4] XOR select_op;
-			A'[5] = A[5] XOR select_op;
-			A'[6] = A[6] XOR select_op;
-			A'[7] = A[7] XOR select_op;
-			
-			A_9th' = A_9th XOR select_op;
+    logic [7:0] Sum;
+    logic Sum_9th;
+    logic cout;
 
-			B'[0] = B[0] XOR select_op;
-			B'[1] = B[1] XOR select_op;
-			B'[2] = B[2] XOR select_op;
-			B'[3] = B[3] XOR select_op;
-			B'[4] = B[4] XOR select_op;
-			B'[5] = B[5] XOR select_op;
-			B'[6] = B[6] XOR select_op;
-			B'[7] = B[7] XOR select_op;
-			B_9th' = B_9th XOR select_op;
-			
+		logic C0, C1;
+
+    // Build XOR network to conditionally flip S input and add 1 via the
+    // carry in.
+		always_comb begin
+			S'[0] = S[0] XOR select_op;
+			S'[1] = S[1] XOR select_op;
+			S'[2] = S[2] XOR select_op;
+			S'[3] = S[3] XOR select_op;
+			S'[4] = S[4] XOR select_op;
+			S'[5] = S[5] XOR select_op;
+			S'[6] = S[6] XOR select_op;
+			S'[7] = S[7] XOR select_op;
+
+			S_9th' = S_9th XOR select_op;
+
 			cin = 1b'0 XOR select_op;
+
+      // Building logic to by pass adders if M is 0, i.e. LSB of B is 0
+      // so don't add.
+      if(M)
+        begin
+          Final_Sum = Sum;
+          Final_Sum_9th = Sum_9th;
+          COUT = cout;
+        end
+      else
+        begin
+          Final_Sum = A[7:0];
+          Final_Sum_9th = A_9th;
+          COUT = X;
+        end
 		end
-		
-		
-		four_bit_ra FRA0(.x(A'[3 : 0]), .y(B'[3 : 0]), .cin(cin), .s(Sum[3 : 0]), .cout(C0));
-		four_bit_ra FRA1(.x(A'[7 : 4]), .y(B'[7 : 4]), .cin(C0), .s(Sum[7 : 4]), .cout(C1));
-		full_adder(.x(A_9th'), .y(B_9th'), .cin(C1), .s(Sum_9th), .cout(cout));
-	
+
+    // Instantiating the 4-bit ripple adders and a one final full adder for
+    // the 9th bit.
+		four_bit_ra FRA0(.x(S'[3 : 0]), .y(A[3 : 0]), .cin(cin), .s(Sum[3 : 0]), .cout(C0));
+		four_bit_ra FRA1(.x(S'[7 : 4]), .y(A[7 : 4]), .cin(C0), .s(Sum[7 : 4]), .cout(C1));
+		full_adder(.x(S_9th'), .y(A_9th), .cin(C1), .s(Sum_9th), .cout(cout));
+
 endmodule
 
 
@@ -71,9 +80,9 @@ module four_bit_ra(
 	full_adder fa1(.x(x[1]), .y(y[1]), .cin(c0 ), .s(s[1]), .cout(c1));
 	full_adder fa2(.x(x[2]), .y(y[2]), .cin(c1 ), .s(s[2]), .cout(c2));
 	full_adder fa3(.x(x[3]), .y(y[3]), .cin(c2 ), .s(s[3]), .cout(cout));
-	
-						
-						
+
+
+
 endmodule
 
 module full_adder(
@@ -85,5 +94,5 @@ module full_adder(
 						);
 	assign s    = x^y^cin;
 	assign cout = (x&y) | (y&cin) | (cin&x);
-	
+
 endmodule
--- lab5/control.sv
+++ lab5/control.sv
@@ -1,16 +1,16 @@
-module control (input  logic Clk, Reset, LoadA, LoadB, Execute, M
-                output logic Shift_En, select_op, Ld_A, Ld_B );
+module control (input  logic Clk, Reset, ClearA_LoadB, Execute, M
+                output logic Shift_En, select_op, Ld_A, Ld_B, Clear_A);
 
     // Declare signals curr_state, next_state of type enum
     // with enum values of A, B, ..., F as the state values
 	 // Note that the length implies a max of 8 states, so you will need to bump this up for 8-bits
-    enum logic [4:0] {A0, S0, A1, S1, A2, S2, A3, S3, A4, S4, A5, S5, A6, S6, A7, S7, Finished, Rest}   curr_state, next_state;
+    enum logic [4:0] {A0, S0, A1, S1, A2, S2, A3, S3, A4, S4, A5, S5, A6, S6, A7, S7, Finished, Wait}   curr_state, next_state;
 
 	//updates flip flop, current state is the only one
     always_ff @ (posedge Clk)
     begin
-        if (Reset)
-            curr_state <= A;
+        if (~Reset)
+            curr_state <= Wait;
         else
             curr_state <= next_state;
     end
@@ -19,7 +19,7 @@ module control (input  logic Clk, Reset, LoadA, LoadB, Execute, M
 	always_comb
     begin
 
-		  next_state  = curr_state;	//required because I haven't enumerated all possibilities below
+		    next_state  = curr_state;	//required because I haven't enumerated all possibilities below
         unique case (curr_state)
 
         A0     :    next_state = S0;
@@ -28,18 +28,20 @@ module control (input  logic Clk, Reset, LoadA, LoadB, Execute, M
         S1     :    next_state = A2;
         A2     :    next_state = S2;
         S2     :    next_state = A3;
-		  A3     :    next_state = S3;
-		  S3     :    next_state = A4;
-		  A4     :    next_state = S4;
-		  S4     :    next_state = A5;
+  		  A3     :    next_state = S3;
+  		  S3     :    next_state = A4;
+  		  A4     :    next_state = S4;
+  		  S4     :    next_state = A5;
         A5     :    next_state = S5;
         S5     :    next_state = A6;
         A6     :    next_state = S6;
         S6     :    next_state = A7;
         A7     :    next_state = S7;
         S7     :    next_state = Finish;
-		  Finish :    next_state = Wait; //only if ~RUN
-		  Wait   :    next_state = S0; //only if RUN
+  		  Finish :    if(Execute)
+                      next_state = Wait; //only if execute button is released
+  		  Wait   :    if(~Execute)
+                      next_state = A0; //only if execute button is pressed again
 
         endcase
 
@@ -47,8 +49,69 @@ module control (input  logic Clk, Reset, LoadA, LoadB, Execute, M
         case (curr_state)
 
 		  /* Fill case statements with vals */
-		  A0		:
+		  A0                          	:
+            begin
+              Shift_En  = 1b'0;
+              select_op = 1b'0;
+              Ld_A      = 1b'0;
+              Ld_B      = 1b'0;
+              Clear_A   = 1b'1;
+            end
+      A1, A2, A3, A4, A5, A6	     	:
+            begin
+              Shift_En  = 1b'0;
+              select_op = 1b'0;
+              Ld_A      = 1b'0;
+              Ld_B      = 1b'0;
+              Clear_A   = 1b'0;
+            end
+      S0, S1, S2, S3, S4, S5, S6, S7    :
+            begin
+              Shift_En  = 1b'1;
+              select_op = 1b'0;
+              Ld_A      = 1b'0;
+              Ld_B      = 1b'0;
+              Clear_A   = 1b'0;
+            end
+      A7     :
+        begin
+          Shift_En  = 1b'0;
+          if(M == 1'b0)
+            select_op = 1b'0;
+          else if(M == 1'b1)
+            select_op = 1b'1;
+          else
+            select_op = 1b'0;
+          Ld_A      = 1b'0;
+          Ld_B      = 1b'0;
+          Clear_A   = 1b'0;
 
+        end
+      Finish :
+        begin
+          Shift_En  = 1b'0;
+          select_op = 1b'0;
+          Ld_A      = 1b'0;
+          Ld_B      = 1b'0;
+          Clear_A   = 1b'0;
+        end
+      Wait   :
+        begin
+          Shift_En  = 1b'1;
+          select_op = 1b'0;
+          if(~ClearA_LoadB) // NOT due to active low
+            begin
+              Ld_A      = 1b'0;
+              Ld_B      = 1b'1;
+              Clear_A   = 1b'1;
+            end
+          else
+            begin
+              Ld_A      = 1b'0;
+              Ld_B      = 1b'0;
+              Clear_A   = 1b'1;
+            end
+        end
 		  endcase
 	 end
 
