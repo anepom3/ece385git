0f8ffa3219278d6d101e2480b9f0c0d66ef2e815
tpm2 <tpm2@illinois.edu>
2020-02-16 18:55:03 -0600

    Working through testing. Error for (-128)*(-128)

--- lab5/testbench.sv
+++ lab5/testbench.sv
@@ -22,6 +22,9 @@ timeprecision 1ns;
                   BHexL;
 						
    logic 			X_reg;
+	
+	//Holders for answers
+	logic [15:0] ans_1, ans_2;
  
  
 always begin : CLOCK_GENERATION
@@ -41,22 +44,40 @@ multiplier8x8 tp(.*);
 initial begin : TEST_VECTORS
 
 // Reset
-Reset = 0;
+#1 Reset = 0;
 ClearA_LoadB = 1;
 Run   = 1;
 
 //test case1
+#1 Reset = 1; // Release Reset button
+
+#1 ClearA_LoadB = 0; // Press ClearA_LoadB
+	S     = 8'b00000010; // 2 into Load B
+
+#1 ClearA_LoadB = 1;
+	S		= 8'b10000001; // -127 held on the switches
+	
+#1 ans_1 = 16'hff02; // -254 is the answer
+
+#1 Run   = 0; //check answer at 450ns
+
+// Reset
+#40 Reset = 0;
+#1 ClearA_LoadB = 1;
+#1 Run   = 1;
+
+//test case2
 #2 Reset = 1; // Release Reset button
 
 #2 ClearA_LoadB = 0; // Press ClearA_LoadB
-	S     = 8'b00000001; // 1 into Load B
+	S     = 8'b10000000; // -128 into Load B
 
 #2 ClearA_LoadB = 1;
-	S		= 8'b00000010; // 2 held on the switches
+	S		= 8'b10000000; // -128 held on the switches
 	
-#2 Run   = 0;
+#1 ans_2 = 16'h8000; // 16384 is the answer
 
-#22 ;
+#1 Run   = 0; //check answer at 900ns
 
 end
 
