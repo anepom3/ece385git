7be4aafbf32763e6c23042f95745e610d3b36cd4
tpm2 <tpm2@illinois.edu>
2020-02-21 11:44:08 -0600

    Added some comments for clarity

--- lab6/ECE385_lab6_provided/Mem2IO.sv
+++ lab6/ECE385_lab6_provided/Mem2IO.sv
@@ -14,23 +14,28 @@
 
 
 module  Mem2IO ( 	input logic Clk, Reset,
-					input logic [19:0]  ADDR, 
-					input logic CE, UB, LB, OE, WE,
+					input logic [19:0]  ADDR,
+					input logic CE, UB, LB, OE, WE, // (All ative low, see 6.4-6.5 for detailed descriptions)
+																					// CE - Chip enable
+																					// UB - Upper Byte enable
+																					// LB - Lower Byte enable
+																					// OE - Output enable
+																					// WE - Write enable
 					input logic [15:0]  Switches,
 					input logic [15:0] Data_from_CPU, Data_from_SRAM,
 					output logic [15:0] Data_to_CPU, Data_to_SRAM,
 					output logic [3:0]  HEX0, HEX1, HEX2, HEX3 );
 
 	logic [15:0] hex_data;
-   
+
 	// Load data from switches when address is xFFFF, and from SRAM otherwise.
 	always_comb
-    begin 
+    begin
         Data_to_CPU = 16'd0;
-        if (WE && ~OE) 
-			if (ADDR[15:0] == 16'hFFFF) 
+        if (WE && ~OE)
+			if (ADDR[15:0] == 16'hFFFF)
 				Data_to_CPU = Switches;
-			else 
+			else
 				Data_to_CPU = Data_from_SRAM;
     end
 
@@ -38,13 +43,13 @@ module  Mem2IO ( 	input logic Clk, Reset,
 	assign Data_to_SRAM = Data_from_CPU;
 
 	// Write to LEDs when WE is active and address is xFFFF.
-	always_ff @ (posedge Clk) begin 
-		if (Reset) 
+	always_ff @ (posedge Clk) begin
+		if (Reset)
 			hex_data <= 16'd0;
-		else if ( ~WE & (ADDR[15:0] == 16'hFFFF) ) 
+		else if ( ~WE & (ADDR[15:0] == 16'hFFFF) )
 			hex_data <= Data_from_CPU;
     end
-       
+
 	assign HEX0 = hex_data[3:0];
 	assign HEX1 = hex_data[7:4];
 	assign HEX2 = hex_data[11:8];
