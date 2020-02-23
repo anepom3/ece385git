6deca43daaf5863892bdd00339f9474d62fe23c7
anepom3 <anepom3@illinois.edu>
2020-02-23 15:02:49 -0600

    Added MUX files and added comments to datapath.sv file

--- lab6/ECE385_lab6_provided/datapath.sv
+++ lab6/ECE385_lab6_provided/datapath.sv
@@ -1,7 +1,8 @@
 // TODO we'l make a datapath module here!
 // This will be used to connect all the modules together.
-// PC (Program Counter) Package: PC, PCMUX, '+1', GatePC
-// MDR Package: MDR, GateMDR
-// MAR Package: MAR, GateMAR
+// Week 1
+// PC (Program Counter) Package: PC, PCMUX, '+1', (GatePC)
+// MDR Package: MDR, (GateMDR)
+// MAR Package: MAR, (GateMAR)
 // IR Package: IR
-// 
+// Internal Tri-state MUX Package: GateMDR, GateMAR, GatePC (Gate ALU for Week 2)
