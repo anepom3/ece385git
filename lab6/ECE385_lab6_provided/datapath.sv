8fecb0b9474f37e9dcf5a7bfe5c0ba5249d0aa70
tpm2 <tpm2@illinois.edu>
2020-02-23 19:08:26 -0600

    Added structure to datapath module. Needs to be finished still

--- lab6/ECE385_lab6_provided/datapath.sv
+++ lab6/ECE385_lab6_provided/datapath.sv
@@ -6,3 +6,23 @@
 // MAR Package: MAR, (GateMAR)
 // IR Package: IR
 // Internal Tri-state MUX Package: GateMDR, GateMAR, GatePC (Gate ALU for Week 2)
+module datapath (input logic Clk,
+                 input logic Reset,
+
+                 //Does this need to include all of the load signals as inputs for this???
+
+                 input logic [15:0] D_bus_IN,
+
+                 output logic [15:0] D_bus_OUT);
+
+
+          //What should we put for the LD values for these instantiations???
+          PC PC_inst(.Clk, .Reset, .LD_PC( ??? ), .DIN(D_bus_OUT), .DOUT(D_bus_IN));
+
+          MDR MDR_inst(.Clk, .Reset, .LD_MDR( ??? ), .DIN(D_bus_OUT), .DOUT(D_bus_IN));
+
+          MAR MAR_inst(.Clk, .Reset, .LD_MAR( ??? ), .DIN(D_bus_OUT), .DOUT(D_bus_IN));
+
+          IR IR_inst(.Clk, .Reset, .LD_IR( ??? ), .DIN(D_bus_OUT), .DOUT(D_bus_IN));
+
+endmodule
