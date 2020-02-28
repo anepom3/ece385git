397c17aff21fde8e47cee9f1559212a85625b83a
anepom3 <anepom3@illinois.edu>
2020-02-28 11:24:04 -0600

    Fixed ADDR1MUX. Requires testing

--- lab6/ECE385_lab6_provided/ADDR1MUX.sv
+++ lab6/ECE385_lab6_provided/ADDR1MUX.sv
@@ -4,13 +4,13 @@
 module ADDR1MUX (input logic [15:0] PC,
                                     BaseR,
 
-                 input logic [3:0] Select, // ADDR1MUX control signal
+                 input logic Select, // ADDR1MUX control signal
                  output logic [15:0] Gate_OUT // To ADDR
 );
 
     always_comb begin
           Gate_OUT = 16'hxxxx;
-          case(Select)
+          case(Select) // Check to make sure selction is correct.
               1'b0 : Gate_OUT = PC;
               1'b1 : Gate_OUT = BaseR;
               default : Gate_OUT = 16'hxxxx;
