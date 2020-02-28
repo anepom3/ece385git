46b027ee092d45cb02931ceca1fc2481bf563998
anepom3 <anepom3@illinois.edu>
2020-02-28 11:39:51 -0600

    Finished ADDR2MUX.sv. Testing needed.

--- lab6/ECE385_lab6_provided/ADDR2MUX.sv
+++ lab6/ECE385_lab6_provided/ADDR2MUX.sv
@@ -1,1 +1,22 @@
 // This is the file for the ADDR2MUX!
+// Inputs: 16-bit PC, 16-bit BaseR, Select Bit
+// Outputs: 00 - 16'h0000, 01 - offset6, 10 - PCoffset9, 11 - PCoffset11
+module ADDR2MUX (input logic [15:0] offset6, PCoffset9, PCoffset11,
+
+                 input logic [1:0] Select, // ADDR2MUX control signal
+                 output logic [15:0] Gate_OUT // To ADDR
+);
+
+    always_comb begin
+          Gate_OUT = 16'hxxxx;
+          case(Select) // Check to make sure selction is correct.
+              2'b00 : Gate_OUT = 16'h0000;
+              2'b01 : Gate_OUT = offset6;
+              2'b10 : Gate_OUT = PCoffset9;
+              2'b11 : Gate_OUT = PCoffset11;
+
+              default : Gate_OUT = 16'hxxxx;
+          endcase
+    end
+
+endmodule
