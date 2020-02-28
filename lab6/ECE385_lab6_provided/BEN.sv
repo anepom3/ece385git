f4dde646e0c1451edfa0fcfa2f1a43c372c9a8d2
anepom3 <anepom3@illinois.edu>
2020-02-28 11:59:31 -0600

    Finished BEN.sv. Testing needed.

--- lab6/ECE385_lab6_provided/BEN.sv
+++ lab6/ECE385_lab6_provided/BEN.sv
@@ -1,1 +1,16 @@
 // This file is for the BEN register!
+module BEN (// Inputs
+                   input logic Clk, LD_BEN, Reset,
+                   input logic DIN,
+                   // Outputs
+                   output logic DOUT
+                  );
+
+always_ff @ (posedge Clk ) begin
+  if(Reset)
+    DOUT <= 1'b0;
+  else if (LD_IR)
+    DOUT <= DIN;
+end
+
+endmodule
