8c40f2198c6bf826a1eec29f64322881fcb2e253
anepom3 <anepom3@illinois.edu>
2020-03-22 18:08:28 -0500

    Got lab8 step 1-2 done. Compiles and programs to board.

--- lab8/lab8_combined/hpi_io_intf.sv
+++ lab8/lab8_combined/hpi_io_intf.sv
@@ -1,11 +1,11 @@
 // Interface between NIOS II and EZ-OTG chip
 module hpi_io_intf( input        Clk, Reset,
-                    input [1:0]  from_sw_address,
+                    input [1:0]  from_sw_address, // sw i.e. "software"...
                     output[15:0] from_sw_data_in,
                     input [15:0] from_sw_data_out,
                     input        from_sw_r, from_sw_w, from_sw_cs, from_sw_reset, // Active low
                     inout [15:0] OTG_DATA,
-                    output[1:0]  OTG_ADDR,
+                    output[1:0]  OTG_ADDR, // Selecting HPI Registers {Data, Mailbox, Address, Status}
                     output       OTG_RD_N, OTG_WR_N, OTG_CS_N, OTG_RST_N // Active low
                    );
 
