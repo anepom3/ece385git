a811d9d2ff36556de5f2c9b3355942be777914d2
anepom3 <anepom3@illinois.edu>
2020-03-28 17:40:23 -0500

    Added the lab9 edited folder and initialized .qsys file

---
+++ lab9/Lab9_Avalon_Edited_2.0/lab9_soc/lab9_soc_bb.v
@@ -0,0 +1,30 @@
+
+module lab9_soc (
+	clk_clk,
+	nios2_gen2_0_custom_instruction_master_readra,
+	reset_reset_n,
+	sdram_clk_clk,
+	sdram_wire_addr,
+	sdram_wire_ba,
+	sdram_wire_cas_n,
+	sdram_wire_cke,
+	sdram_wire_cs_n,
+	sdram_wire_dq,
+	sdram_wire_dqm,
+	sdram_wire_ras_n,
+	sdram_wire_we_n);	
+
+	input		clk_clk;
+	output		nios2_gen2_0_custom_instruction_master_readra;
+	input		reset_reset_n;
+	output		sdram_clk_clk;
+	output	[12:0]	sdram_wire_addr;
+	output	[1:0]	sdram_wire_ba;
+	output		sdram_wire_cas_n;
+	output		sdram_wire_cke;
+	output		sdram_wire_cs_n;
+	inout	[15:0]	sdram_wire_dq;
+	output	[1:0]	sdram_wire_dqm;
+	output		sdram_wire_ras_n;
+	output		sdram_wire_we_n;
+endmodule
